70399dcf73b1884fd69589a0f35dd37d5fd3c379
Mingazizov Nicita <113692942+Killer-Qwin@users.noreply.github.com>
2023-02-06 14:51:23 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,192 @@
+//lvl1
+// let p = document.querySelector(`p`)
+// let elem = document.querySelector('#elem');
+// elem.addEventListener(`blur`, ()=>{
+//     p.textContent = elem.value
+// })
+
+//lvl2
+// let elem = document.querySelector('#elem');
+// let but = document.querySelector(`#but1`)
+// but1.addEventListener(`click`, ()=>{
+//     if(elem.disabled){
+//         elem.disabled = false;
+//     }else{
+//         elem.disabled = true
+//     }    
+// })
+
+//lvl3
+// let elem = document.querySelector('#elem');
+// let but1 = document.querySelector(`#but1`)
+// let but2 = document.querySelector(`#but2`)
+// but1.addEventListener(`click`, fanc1)
+// but2.addEventListener(`click`, fanc2)   
+// function fanc1(){
+//     elem.removeAttribute(`disabled`);
+// }
+// function fanc2(){
+//      elem.setAttribute(`disabled`, true);
+// }
+//lvl4
+// let elem = document.querySelector('#elem');
+// let but1 = document.querySelector(`#but1`)
+// but1.addEventListener(`click`, ()=>{
+//     console.log(elem.disabled);
+// })
+//lvl5
+// let elem = document.querySelector('#elem1');
+// let but1 = document.querySelector(`#but1`)
+// but1.addEventListener(`click`, ()=>{
+//     if(elem.checked){
+//         elem.checked = false
+//     }else{
+//         elem.checked = true
+//     }
+// })
+
+//lvl6
+// let p = document.querySelector(`p`)
+// let elem = document.querySelector('#elem1');
+// let but1 = document.querySelector(`#but1`)
+// but1.addEventListener(`click`, ()=>{
+//     if(elem.checked == true){
+//         p.textContent = `привет`
+//     }else{
+//         p.textContent = `пока`
+//     }
+// })
+//lvl7
+// let elem = document.querySelector('#elem1');
+// let but = document.querySelector(`#but1`)
+// but.addEventListener(`click`, ()=>{
+//     elem.checked = !elem.checked;
+// })
+//lvl8
+// let radios = document.querySelectorAll('input[type="radio"]');
+// let button = document.querySelector('#but1');
+// let p = document.querySelector(`p`)
+// button.addEventListener('click', function() {
+// 	for (let radio of radios) {
+// 		if (radio.checked) {
+// 			p.textContent = radio.value
+            
+// 		}
+// 	}
+// });
+//lvl9
+// let elem = document.querySelector('#elem');
+// let p = document.querySelector('p');
+
+// elem.addEventListener('change', function() {
+// 	p.textContent = this.value;
+// });
+//lvl10
+// let elem = document.querySelector('#elem1');
+// let p = document.querySelector('p');
+
+// elem.addEventListener('change', function() {
+// 	p.textContent = elem.checked;
+// });
+//lvl11
+// let elem = document.querySelector(`#elem`)
+// elem.addEventListener(`change`, ()=>{
+//         if(elem.value.length <= 5){
+//             elem.style.backgroundColor = `green`
+//         }else{
+//             elem.style.backgroundColor = `red`
+//         }   
+// })
+//lvl12
+// let elem = document.querySelector(`#elem`)
+// elem.addEventListener(`input`, ()=>{
+//         if(elem.value.length > 5){
+//             alert(`угомонись порося`)
+//         }else{
+            
+//         }   
+// })
+
+//lvl13
+// let elem = document.querySelector(`#elem`)
+// let p = document.querySelector(`p`)
+// elem.addEventListener(`input`, ()=>{
+//     for(let i = 0; i <= elem.value.length; i++){
+//         if(elem.value.length < 5){
+//             p.textContent = 5 - i
+//         }else{
+//             p.textContent = i - 5
+//         }
+//     }
+// })
+
+//lvl14
+// let elem1 = document.querySelector(`#elem1`)
+// let elem2 = document.querySelector(`#elem2`)
+// elem1.addEventListener(`input`, ()=>{
+//    if(elem1.value.length >= 2){
+//     elem2.focus();
+//    }
+// })
+// elem2.addEventListener('input', ()=>{
+//     if(elem2.value.length >= 2){
+//         elem2.blur();
+//        }
+// })
+//lvl15
+// let p = document.querySelector(`p`)
+// let select = document.querySelector('#select');
+// select.addEventListener('change', function(){
+// 	p.textContent = this.value
+// });
+
+//lvl16
+// let p = document.querySelector(`p`)
+// let select = document.querySelector('#select');
+// select.addEventListener('change', function(){
+// 	if(this.value % 4 == 0){
+//         p.textContent = `высокосный год`
+//     }else{
+//         p.textContent = `ne высокосный год`
+//     }
+    
+// });
+//lvl17
+// let p = document.querySelector(`p`)
+// let select = document.querySelector('#select');
+// select.addEventListener('change', function(){
+// 	if(this.valu - 5 > 3){
+//         p.textContent = `одых`
+//     }else{
+//         p.textContent = `тоже одых`
+//     }
+    
+// });
+
+//lvl18
+// let select = document.querySelector('#select');
+// select.addEventListener('change', function(){
+//     select.value = this.value
+// });
+
+//lvl19
+// let select = document.querySelector('#select');
+// for(let elem of select){
+//     elem.textContent = elem.textContent + ' ' + elem.value
+// }
+
+//lvl20
+// let select = document.querySelector('#select');
+// select.addEventListener('change', function(){
+//     for(let elem of select)
+//  elem.textContent = elem.textContent + '!';
+// })
+
+//lvl21
+// let select = document.querySelector('#select');
+// let button = document.querySelector('#but1');
+// button.addEventListener('click', function(){
+//  document.write(select.value)
+// })
+
+
